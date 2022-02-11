4727e6c2b2900837640128dec9bdbcacaeb39840
jzorrilla23 <jzorrilla@intellisys.com.do>
2022-02-10 21:01:48 -0400

    Added the method createTask to Tasks class

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('colors');
-const { inquirerMenu, pause } = require('./helpers/inquirer');
+const inquirer = require('inquirer');
+const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
 const { pausa } = require('./helpers/messages');
 const Task = require('./models/task');
 const Tasks = require('./models/tasks');
@@ -8,9 +9,21 @@ console.clear();
 
 const main = async() => {
     let opt = '';
+    const tasks = new Tasks();
     do{
         opt = await inquirerMenu();
-        console.log(opt)
+        
+        switch (opt) {
+            case '1':
+                const desc = await readInput('Add the description: ');
+                tasks.createTask(desc);
+                break;
+            case '2':
+                console.log(tasks._list);
+                break;
+            default:
+                break;
+        }
         if(opt !== '0')
             await pause();
     } while(opt !== '0')
