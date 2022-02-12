fa58ffdd8c8235eb2f792ace66878267a0e79799
jzorrilla23 <jzorrilla@intellisys.com.do>
2022-02-12 19:12:52 -0400

    Show the complete and pending tasks

--- models/tasks.js
+++ models/tasks.js
@@ -1,35 +1,57 @@
 const Task = require('./task');
 
-class Tasks{
+class Tasks {
     _list = {};
 
     get listArr() {
-        return Object.entries(this._list).map((task)=>(
+        return Object.entries(this._list).map((task) => (
             task[1]
         ));
     }
 
-    constructor(){
+    constructor() {
         this._list = {};
     }
 
-    loadTasksFromArray( tasks = []){
-        tasks.forEach( task => {
+    loadTasksFromArray(tasks = []) {
+        tasks.forEach(task => {
             this._list[task.id] = task;
         })
     }
 
-    createTask(desc = ''){
+    createTask(desc = '') {
         const task = new Task(desc);
-        this._list[task.id] = task; 
+        this._list[task.id] = task;
     }
 
-    listComplete(){
-        Object.entries(this._list).forEach( (element,i) =>{
-            let index = String(i +  1).green;
-            const {desc, completeAt} = element[1]
-            let complete = completeAt == null ? 'pendiente'.red : 'complete'.green;
-            console.log(` ${index } ${desc} \t:: ${complete}`)
+    listComplete() {
+        Object.entries(this._list).forEach((element, i) => {
+            let index = String(i + 1).green;
+            const { desc, completedAt } = element[1]
+            let complete = completedAt == null ? 'pendiente'.red : 'complete'.green;
+            console.log(` ${index} ${desc} \t:: ${complete}`)
+        })
+    }
+
+
+    listCompletePending(complete = true) {
+        let count = 0;
+        Object.entries(this._list).forEach(task => {
+           // console.log(task)
+            let { desc, completedAt } = task[1]; 
+            if (complete) {
+                //console.log(task);
+                if(completedAt != null){
+                    count += 1;
+                    console.log(` ${count} ${desc} \t:: ${'complete'.green}`)
+                }
+            }
+            else {
+                if(completedAt == null){
+                    count += 1;
+                    console.log(` ${count} ${desc} \t:: ${'pending'.red}`)
+                }
+            }
         })
     }
 }
